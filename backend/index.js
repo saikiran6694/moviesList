4aa11f720d74fe02311ad46014c6bfbe8be98b7f
saikiranbembalge <saikiranbembalge123@gmail.com>
2023-03-26 19:13:27 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,12 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
   next();
 });
 
